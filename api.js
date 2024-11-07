69098b42cfa14f79300a8acea9fd273ced9dcb86
Melissa <melissa.pinto@alunoescolassociais.org.br>
2024-11-07 09:37:03 -0300

    nao lembro

--- api.js
+++ api.js
@@ -1,4 +1,4 @@
-const apiKey = '6e15ede0aa45b78d0d50d643'
+const apiKey = '6e15ede0aa45b78d0d50d643';
 const apiURL = `https://v6.exchangerate-api.com/v6/${apiKey}/latest/`;
  
 // FUNÇÃO PARA BUSCAR A CHAVE DE CÂMBIO DA API
@@ -7,7 +7,7 @@ async function getExchangeRate(daMoeda, paraMoeda){
  
     try{
 
-       const response = await fetch(´ ${apiURL}${daMoeda} ´); // TRAZENDO O LINK DE API
+       const response = await fetch(`${apiURL}${daMoeda}`); // TRAZENDO O LINK DE API
        const data = await response.json();
 
        if(data.result === 'sucess'){
@@ -28,5 +28,29 @@ async function getExchangeRate(daMoeda, paraMoeda){
    
 }
  
+document.getElementById('currency-form').addEventListener('submit', async function(event){
+
+    const valor = parseFloat(document.getElementById('amount').value);
+    const daMoeda = document.getElementById('daMoeda').value;
+    const paraMoeda = document.getElementById('paraMoeda').value;
+
+    const getExchangeRate = await getExchangeRate(daMoeda, paraMoeda);
+
+    if(exchangeRate){
+
+       const convertedValue = valor * getExchangeRate;
+
+       // EXIBIR RESULTADO
+       const conversao = document.getElementById('conversao');
+       conversao.textContent = `Resultado: ${convertedValue.toFixed(2)} ${paraMoeda}`;
+   
+    }else{
+
+      alert('Erro ao buscar a cotação. Tente novamente.')
+
+    }
+    
+
+});
 
 
