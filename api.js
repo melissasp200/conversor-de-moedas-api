9c7247584e4cc80529a1b80cd3e01f76b2d9a8b4
Melissa <melissa.pinto@alunoescolassociais.org.br>
2024-11-07 10:47:18 -0300

    correcao do codigo

--- api.js
+++ api.js
@@ -1,55 +1,59 @@
 const apiKey = '6e15ede0aa45b78d0d50d643';
 const apiURL = `https://v6.exchangerate-api.com/v6/${apiKey}/latest/`;
- 
+
 // FUNÇÃO PARA BUSCAR A CHAVE DE CÂMBIO DA API
- 
-async function getExchangeRate(daMoeda, paraMoeda){
- 
-    try{
 
-       const response = await fetch(`${apiURL}${daMoeda}`); // TRAZENDO O LINK DE API
-       const data = await response.json();
+async function getExchangeRate(daMoeda, paraMoeda) {
 
-       if(data.result === 'sucess'){
+    try {
+
+        const response = await fetch(`${apiURL}${daMoeda}`); // TRAZENDO O LINK DE API
+        const data = await response.json();
+
+        if (data.result === 'success') {
             return data.conversion_rates[paraMoeda]; //RETORNO O VALOR DA MOEDA
 
-       }else{
+        } else {
 
-        throw new Error('Erro ao buscar a taxa de câmbio');
+            throw new Error('Erro ao buscar a taxa de câmbio');
 
-       }
+        }
 
-    }catch(error){
+    } catch (error) {
         console.error("Erro: ", error);
         return null;
 
 
     }
-   
+
 }
- 
-document.getElementById('currency-form').addEventListener('submit', async function(event){
+// OBTER VALORES
+
+document.getElementById('currency-form').addEventListener('submit', async function(event) {
+    event.preventDefault();
 
     const valor = parseFloat(document.getElementById('amount').value);
     const daMoeda = document.getElementById('daMoeda').value;
     const paraMoeda = document.getElementById('paraMoeda').value;
 
-    const getExchangeRate = await getExchangeRate(daMoeda, paraMoeda);
 
-    if(exchangeRate){
+    // BUSCAR CAMBIO
+    const exchangeRate = await getExchangeRate(daMoeda, paraMoeda);
+    
+    if (exchangeRate) {
 
-       const convertedValue = valor * getExchangeRate;
+        const convertedValue = valor * exchangeRate;
 
-       // EXIBIR RESULTADO
-       const conversao = document.getElementById('conversao');
-       conversao.textContent = `Resultado: ${convertedValue.toFixed(2)} ${paraMoeda}`;
-   
-    }else{
+        // EXIBIR RESULTADO
+        const conversao = document.getElementById('conversao');
+        conversao.textContent = `Resultado: ${convertedValue.toFixed(2)} ${paraMoeda}`;
 
-      alert('Erro ao buscar a cotação. Tente novamente.')
+    } else {
+
+        alert('Erro ao buscar a cotação. Tente novamente.')
 
     }
-    
+
 
 });
 
